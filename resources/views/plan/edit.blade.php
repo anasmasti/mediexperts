eb0548d45fa4041f22053ece17c318dfb57a167f
badrelazzaby <badr.azzaby411@gmail.com>
2021-03-12 10:27:29 +0100

    12/03/2021__

--- resources/views/plan/edit.blade.php
+++ resources/views/plan/edit.blade.php
@@ -96,12 +96,12 @@
                 <label for="l_tierpay_PF" class="custom-control-label">Lettre de tiers-payant PF</label>
                 </div>
                 <div class="custom-control custom-checkbox">
-                <input type="checkbox" name="at_approb_ds" id="at_approb_ds" class="custom-control-input" {{ (mb_strtolower($plans->at_approb_ds)=="préparé") ? 'checked' : '' }}>
-                <label for="at_approb_ds" class="custom-control-label">Attestation d'approbation</label>
+                <input type="checkbox" name="at_approb_if" id="at_approb_if" class="custom-control-input" {{ (mb_strtolower($plans->at_approb_if)=="préparé") ? 'checked' : '' }}>
+                <label for="at_approb_if" class="custom-control-label">Attestation d'approbation IF</label>
                 </div>
                 <div class="custom-control custom-checkbox">
-                <input type="checkbox" name="rpt_DS_PFOPT" id="rpt_DS_PFOPT" class="custom-control-input" {{ (mb_strtolower($plans->rpt_DS_PFOPT)=="préparé") ? 'checked' : '' }}>
-                <label for="rpt_DS_PFOPT" class="custom-control-label">Rapport DS</label>
+                <input type="checkbox" name="at_approb_ds" id="at_approb_ds" class="custom-control-input" {{ (mb_strtolower($plans->at_approb_ds)=="préparé") ? 'checked' : '' }}>
+                <label for="at_approb_ds" class="custom-control-label"> Attestation d'approbation DS </label>
                 </div>
                 <div class="custom-control custom-checkbox">
                 <input type="checkbox" name="rpt_IF_PFOPT" id="rpt_IF_PFOPT" class="custom-control-input" {{ (mb_strtolower($plans->rpt_IF_PFOPT)=="préparé") ? 'checked' : '' }}>
