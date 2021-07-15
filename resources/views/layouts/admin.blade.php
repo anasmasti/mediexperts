88318debfe78674e21e3fc283c6895b5fe190a5d
anasmasti <anasmasti@hotmail.com>
2021-07-15 17:29:22 +0100

    Add new functionalities from dev branch

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -710,7 +710,7 @@
       <strong class="">Mediexperts </strong>
       <strong>{{ date('Y') }}</strong>
       <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> <strong>2.2.1:a</strong>
+      <b>Version</b> <strong>2.2.1:a1</strong>
       </div>
     </footer>
 
