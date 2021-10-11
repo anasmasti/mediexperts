f74ef91c281f9f9358f309065c80acd3518ed369
anasmasti <anasmasti@hotmail.com>
2021-10-11 12:19:33 +0100

    Merge new changes from dev branch and change version name

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -710,7 +710,7 @@
       <strong class="">&copy; Mediexperts</strong>
       <strong>{{ date('Y') }}</strong>
       <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> <strong>2.4</strong>
+      <b>Version</b> <strong>2.4.1</strong>
       </div>
     </footer>
 
