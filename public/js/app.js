c2aff8c1ccab21b6b1d842d3097d0bf3769e26fb
anasmasti <anasmasti@hotmail.com>
2022-01-20 13:03:00 +0100

    Fix update action plan formation

--- public/js/app.js
+++ public/js/app.js
@@ -74438,8 +74438,8 @@ var state = {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\HP\Desktop\mediexperts\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\HP\Desktop\mediexperts\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\anasm\Documents\Projects\Work\Mediexperts_App\app\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\anasm\Documents\Projects\Work\Mediexperts_App\app\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
