70a216dfd1f9143e5caf1cc95b8fb7cc1898dce8
anasmasti <anasmasti@hotmail.com>
2022-01-20 13:05:49 +0100

    Change version name

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -715,7 +715,7 @@
       <strong class="">&copy; Mediexperts</strong>
       <strong>{{ date('Y') }}</strong>
       <div class="float-right d-none d-sm-inline-block">
-      <b>Version : </b> <strong>2.6.3</strong>
+      <b>Version : </b> <strong>2.6.4</strong>
       </div>
     </footer>
 
